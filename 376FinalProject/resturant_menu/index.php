6fa271d8218c012bf56e39d4605ac8202db5168b
Omari Chatman <omarichatman@users.noreply.github.com>
2019-04-12 16:33:41 -0400

    Add files via upload

---
+++ 376FinalProject/resturant_menu/index.php
@@ -0,0 +1,36 @@
+<!-- <?php include 'view/header.php'; ?> -->
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Restaurant</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<main>
+    <nav>
+
+    <div>
+        <img src='https://prodimages.restaurants-sign.com/350/n515079-food-and-drink-restaurant-logo-neon-sign.jpg' />
+    </div>
+
+    <form action="/action_page.php">
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    
+    </nav>
+</main>
+</body>
+</html>
+
+<!-- <?php include 'view/footer.php'; ?> -->
